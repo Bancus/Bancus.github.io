213cadcb462007f65fc4e3049ba5bc3dc2f7d33c
Benedict Asamoah <wrilben@icloud.com>
2017-10-16 23:12:42 +0000

    Web template

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("#clients-logo").owlCarousel({
+		autoPlay: 3000,
+		items : 5,
+		itemsDesktop : [1199,5],
+		itemsDesktopSmall : [979,5],
+	});
+	$("#testimonial-slider").owlCarousel({
+	    paginationSpeed : 1000,      
+	    singleItem:true,
+	    autoPlay: 3000,
+	    autoplayHoverPause : true,
+	});
+});
+
+
+
